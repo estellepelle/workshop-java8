d84c1bd0300e8812c8bd7fd3da6072f3d4a3327e
Estelle PELLE <estellepelle9261@gmail.com>
2017-04-13 11:02:08 +0200

    exos valide 1 et 2 sur les streams

--- src/test/java/java8/ex01/Stream_01_Test.java
+++ src/test/java/java8/ex01/Stream_01_Test.java
@@ -136,14 +136,14 @@ public class Stream_01_Test {
 
     @Test
     public void test_stream_min() throws Exception {
-        List<Order> orders = new Data().getOrders();
+        
 
         List<Pizza> pizzas = new Data().getPizzas();
 
         // TODO Trouver la pizza la moins chère dont le prix est >= 950
         Optional<Pizza> result = pizzas.stream()
         		.filter(p->p.getPrice()>= 950)
-        		.min((p1,p2)-> p1.getPrice().compareTo(p2.getPrice()));
+        		.min(Comparator.comparing(Pizza::getPrice));
 
         assertThat(result.isPresent(), is(true));
         assertThat(result.get(), hasProperty("id", is(3)));
--- src/test/java/java8/ex02/Stream_02_Test.java
+++ src/test/java/java8/ex02/Stream_02_Test.java
@@ -24,7 +24,10 @@ public class Stream_02_Test {
         List<Order> orders = new Data().getOrders();
 
         // Trouver la liste des clients ayant déjà passés une commande
-        List<Customer> result = null;
+        List<Customer> result = orders.stream()
+        		.map(Order::getCustomer).distinct()
+        		.collect(Collectors.toList());
+        		
 
         assertThat(result, hasSize(2));
     }
@@ -36,7 +39,11 @@ public class Stream_02_Test {
 
         // TODO calculer les statistiques sur les prix des pizzas vendues
         // TODO utiliser l'opération summaryStatistics
-        IntSummaryStatistics result = null;
+        IntSummaryStatistics result = orders.stream()
+        		.flatMap(o->o.getPizzas().stream())
+        		.mapToInt(Pizza::getPrice)
+        		.summaryStatistics();
+        		
 
 
         assertThat(result.getSum(), is(10900L));
